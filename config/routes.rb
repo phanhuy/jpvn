ac92670284a872082f78acf4c73c5e4f3b890a55
phunghangbk1 <tranthique82@gmail.com>
2015-10-31 08:05:28 +0000

    follow user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,13 @@ Rails.application.routes.draw do
   root 'static_pages#home'
 
   devise_for :users
+  
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
