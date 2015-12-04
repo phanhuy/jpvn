b671471150aac7a8da518989d0e351ba31a3f8fe
phanhuy <phanhuy.bkhn@gmail.com>
2015-12-04 16:10:47 +0000

    task#46 Add css to Test and word learning

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,19 @@
 class StaticPagesController < ApplicationController
   def home
     if user_signed_in?
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 6)
+      
+      
+      @goals = current_user.goals
+    
+    
+      #current goal:
+      @goal = current_user.goals.where("deadline > ?",Time.now).first
+      #byebug
+      if @goal != nil
+        @tests = current_user.tests.where("mark >= 0 and created_at > ? and created_at < ?",@goal.created_at,@goal.deadline).order('created_at ASC')
+      end
+      
     end
   end
 end
